714c96524b03d79b1c69c88fb55b4549b6f21e5b
Rolando <74209303+rbcuellar98@users.noreply.github.com>
2023-04-06 16:26:23 -0400

    Added database and first table using prisma

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main>
-      <button className="flex justify-center ">Hello Next</button>
+      <button className="flex justify-center items-center min-h-screen">Hello Next</button>
       <div className="bg-red-500">This div has a red background color</div>
 
     </main>
