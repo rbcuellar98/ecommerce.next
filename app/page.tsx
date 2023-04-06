beffc6c8a43aeba131665456a6807261d06081ef
Rolando <74209303+rbcuellar98@users.noreply.github.com>
2023-04-06 15:26:17 -0400

    file changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello Next</h1>
+      <h1 className="text-2xl">Hello Next</h1>
+      <div className="bg-red-500">This div has a red background color</div>
+
     </main>
   )
 }
