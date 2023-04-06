d026683485b298726d91e86a1b0e9da0687b5564
Rolando <74209303+rbcuellar98@users.noreply.github.com>
2023-04-06 15:42:45 -0400

    clear

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-2xl">Hello Next</h1>
+      <button className="flex justify-center ">Hello Next</button>
       <div className="bg-red-500">This div has a red background color</div>
 
     </main>
