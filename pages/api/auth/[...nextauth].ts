11ef1cdf66e9811f72fb4eb9ea7f2c24e81b1cb0
Rolando <74209303+rbcuellar98@users.noreply.github.com>
2023-04-06 22:12:33 -0400

    implemented stripe api for payment

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
+import Stripe from 'stripe'
 
 
 
@@ -17,4 +18,23 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  events:{
+    createUser: async({user}) => {
+        const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+            apiVersion: '2022-11-15'
+        })
+        // Create a stripe costumer
+        if(user.name && user.email){
+        const costumer = await stripe.customers.create({
+            email: user.email,
+            name: user.name,
+        })
+        // Also update the prisma user with the stripeid
+        await prisma.user.update({
+            where: {id: user.id},
+            data: {stripeCustomerId: costumer.id}
+        })
+    }
+    },
+  },
 })
